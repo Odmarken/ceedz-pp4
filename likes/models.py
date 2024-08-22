a60d45569ff20d5943f88dd032d2db3afe1f863c
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-08-22 13:22:00 +0000

    added conctact form and login success

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from blog.models import Post
 
-# Create your models here.
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} likes {self.post.title}"
