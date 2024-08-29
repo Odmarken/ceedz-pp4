8bf4d372ba0c27782a01f1ac331726de950f14c4
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-08-29 18:12:56 +0000

    added images folder

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,19 @@
+# blog/models.py
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)  
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[self.slug])
-
-    class Meta:
-        ordering = ['-created_at']
