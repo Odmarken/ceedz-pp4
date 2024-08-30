6ab36bebf261360d296affcb82f805548b303bbe
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-08-30 19:21:23 +0000

    added the handlers for urls.py for error code

--- ceedz/urls.py
+++ ceedz/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from blog.views import post_list  
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import handler404, handler500, handler403, handler400
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,7 +17,12 @@ urlpatterns = [
     path('', post_list, name='home'),  
 ]
 
-# media section
-
+# Media section
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Error handlers
+handler404 = 'ceedz.views.custom_404'
+handler500 = 'ceedz.views.custom_500'
+handler403 = 'ceedz.views.custom_403'
+handler400 = 'ceedz.views.custom_400'
